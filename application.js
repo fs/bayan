8d202bf213640313077e2304f0a2594cf56f44e1
Nickolay Abdrafikov <nicck.olay@gmail.com>
2014-01-17 11:27:09 +0400

    FormBuilder class (and prototypes)

--- application.js
+++ application.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   var friends = $('.builder.friends');
-  var formBuilderFriends = makeFormBuilderFor(friends);
-  // TODO: var formBuilderFriends = new FormBuilder(friends);
+  var formBuilderFriends = new FormBuilder(friends);
   formBuilderFriends.start(friends);
 
   var collegues = $('.builder.collegues');
-  var formBuilderCollegues = makeFormBuilderFor(collegues);
+  var formBuilderCollegues = new FormBuilder(collegues);
   formBuilderCollegues.start(collegues);
 });
--- builder.js
+++ builder.js
@@ -1,55 +1,48 @@
-// TODO:
-// function FormBuilder() {
-//   // ...
-// }
-
-function makeFormBuilderFor(element) {
-  var formBuilder = {
-    element: null,
-
-    start: function(element) {
-      console.log('start ' + formBuilder.element[0].className);
-
-      element.find('.buttons .button').click(function() {
-        var button = $(this);
-        formBuilder.buttonClicked(button);
-      });
-
-      element.find('.fields .close').click( formBuilder.closeClicked );
-    },
-
-    buttonClicked: function(button) {
-      button.addClass('on');
-
-      var fieldName = button.data('field');
-      var field = formBuilder._getFieldByName(fieldName);
-
-      field.addClass('on');
-    },
-
-    closeClicked: function() {
-      var field = $(this).parent();
-      var fieldName = field.data('field');
-
-      var button = formBuilder._getButtonByName(fieldName);
-
-      field.removeClass('on');
-      button.removeClass('on');
-    },
-
-    _getFieldByName: function(fieldName) {
-      var field = formBuilder.element.find('.fields .field[data-field="'+fieldName+'"]');
-
-      return field;
-    },
-
-    _getButtonByName: function(fieldName) {
-      return $('.builder .buttons .button[data-field="'+fieldName+'"]');
-    }
-
-  };
-
-  formBuilder.element = element;
-  return formBuilder;
+function FormBuilder(element) {
+  this.element = element;
 }
 
+FormBuilder.prototype.start = function(element) {
+  console.log('start ' + this.element[0].className);
+
+  var formBuilder = this;
+
+  this.element.find('.buttons .button').click(function() {
+    var button = $(this);
+    formBuilder.buttonClicked(button);
+  });
+
+  this.element.find('.fields .close').click(function() {
+    var el = $(this);
+    formBuilder.closeClicked(el);
+  });
+}
+
+FormBuilder.prototype.buttonClicked = function(button) {
+  button.addClass('on');
+
+  var fieldName = button.data('field');
+  var field = this._getFieldByName(fieldName);
+
+  field.addClass('on');
+}
+
+FormBuilder.prototype.closeClicked = function(el) {
+  var field = $(el).parent();
+  var fieldName = field.data('field');
+
+  var button = this._getButtonByName(fieldName);
+
+  field.removeClass('on');
+  button.removeClass('on');
+}
+
+FormBuilder.prototype._getFieldByName = function(fieldName) {
+  var field = this.element.find('.fields .field[data-field="'+fieldName+'"]');
+
+  return field;
+}
+
+FormBuilder.prototype._getButtonByName = function(fieldName) {
+  return this.element.find('.buttons .button[data-field="'+fieldName+'"]');
+}
